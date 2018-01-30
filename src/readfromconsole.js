c950e2b1666b710c242d8fab7d6d1f204c00c734
Michell Cronberg <michell@cronberg.dk>
2018-01-30 12:57:23 +0100

    babel eslint

--- src/readfromconsole.js
+++ src/readfromconsole.js
@@ -16,7 +16,7 @@ console.log("Oh, " + userName + " loves " + favFood + "!");
 
 var readlineSync2 = require("readline-sync"),
   animals = ["Lion", "Elephant", "Crocodile", "Giraffe", "Hippo"],
-  index = readlineSync.keyInSelect(animals, "Which animal?");
+  index = readlineSync2.keyInSelect(animals, "Which animal?");
 console.log("Ok, " + animals[index] + " goes to your room.");
 
 var readlineSync3 = require("readline-sync");
--- src/request.js
+++ src/request.js
@@ -5,11 +5,12 @@ var fs = require("fs");
 
 request("http://www.google.com", function(error, response, body) {
   console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-  //console.log("body:", body); // Print the HTML for the Google homepage.
+  console.log("body:", body.substring(0, 50)); // Print the HTML for the Google homepage.
 });
 
 request("http://www.dmi.dk/uploads/tx_dmidatastore/webservice/k/d/_/n/g/femdgn_dk.png").pipe(fs.createWriteStream("temp/femdgn_dk.png"));
 
 request.post({ url: "http://fdemo2.cronberg.dk/SimplePost_submit.cshtml", form: { key: "value" } }, function(err, response, body) {
   console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+  console.log("body:", body.substring(0, 50)); // Print the HTML for the Google homepage.
 });
